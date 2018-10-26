be4f66e80e48a96f400ee30aa9958832581a017c
silviane1 <34492392+silviane1@users.noreply.github.com>
2018-10-25 22:22:07 -0400

    Update CargasDao.java

--- src/main/java/Dao/CargasDao.java
+++ src/main/java/Dao/CargasDao.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dao;
 
-/**
- *
- * @author Harrison
- */
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import java.util.ArrayList;
+import java.util.Scanner; 
+
 public class CargasDao {
     
+        public static final DBObject toDBObject2(Cargas carga){
+        Scanner scan= new Scanner(System.in);
+        
+        System.out.println("Digite o id da carga:");
+        Long id = scan.nextLong();
+        scan.nextLine();
+        System.out.println("Digite o produto:");
+        String produto = scan.nextLine();
+        System.out.println("Digite o tamanho:");
+        double tamanho = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("Digite a origem:");
+        String origem = scan.nextLine();
+        System.out.println("Digite o destino");
+        String destino = scan.nextLine();
+        
+        
+        
+        carga = new Cargas();
+        carga.setId(id);
+        carga.setProdutos(produto);
+        carga.setTamanho(tamanho);
+        carga.setOrigem(origem);
+        carga.setDestino(destino);
+        
+        
+        
+        return new BasicDBObject("Id", carga.getId())
+                .append("Produto", carga.getProdutos())
+                .append("Tamanho", carga.getTamanho())
+                .append("Origem", carga.getOrigem())
+                .append("Destino", carga.getDestino());
+    }
+    
+
 }
